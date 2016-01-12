470b4fe018c03b93fbba6a7ce5305c3b317cee2c
davidmarx <david.marx@datamininglab.com>
2016-01-12 18:29:51 -0500

    Cleaned up some code

--- time_series_anomaly_detection/enron_demo.R
+++ time_series_anomaly_detection/enron_demo.R
@@ -1,3 +1,4 @@
+install.packages("igraphdata")
 library(lubridate)
 library(igraph)
 library(igraphdata)
@@ -24,13 +25,13 @@ for(d in dates2){
 }
 end = Sys.time()
 elaps = end - start
-print(elaps) # 6.5 min
+print(elaps) # 20 seconds
 
 start = Sys.time()
 test = scan_stat(graphs, tau=30)
 end = Sys.time()
 elaps = end - start
-print(elaps) # 3min
+print(elaps) # 10 seconds
 
 
 d_test = as.POSIXct(dates2, origin='1970-1-1')
@@ -38,4 +39,20 @@ plot(d_test, test$stat, type='l')
 rug(ymd("2001-03-20"), col="red", lty=2)
 rug(ymd("2001-8-22"), col='purple', lty=2)
 rug(ymd("2001-10-14"), col='blue', lty=2)
+
+get_peak_details = function(peak_ix){
+  peak_date = d_test[-1][peak_ix]
+  node_id = test$arg_max_v[peak_ix]
+  node = V(graphs[[peak_ix]])[node_id]
+  list(date=peak_date, node=node$Name, node_id=node_id)
+}
+
+details = data.table(t(sapply(which(!is.na(test$stat)), get_peak_details)))
+details$stat = test$stat[!is.na(test$stat)]
+details[order(stat, decreasing=TRUE)][1:3]
+
+#                   date           node node_id     stat
+# 1: 2001-03-22 19:00:00  Jim Schwieger      71 4692.000
+# 2: 2001-08-23 20:00:00    Kenneth Lay      95 3856.000
+# 3: 2001-03-20 19:00:00  Thomas Martin     174 2969.867
 
--- time_series_anomaly_detection/mentions_anomaly.R
+++ time_series_anomaly_detection/mentions_anomaly.R
@@ -2,10 +2,27 @@ library(igraph)
 library(RSQLite)
 library(data.table)
 library(lubridate)
+
+#####################################
+# Define some convenience functions #
+#####################################
+
+epochToDate = function(val){as.POSIXct(val, origin="1970-01-01")}
+
+get_peak_details = function(peak_ix){
+  peak_date = d_seq[-1][peak_ix]
+  node_id = test$arg_max_v[peak_ix]
+  node = V(graphs[[peak_ix]])[node_id] # user_history_bot ... wtf? Weirdness.
+  list(date=peak_date, node=node$name)
+}
+
+############################################
+# Read in dynamic graph data and construct # 
+# projections along regular time intervals #
+############################################
+
 db_path = "subreddit_mentions.db"
 conn = dbConnect(SQLite(), db_path)
-
-toEpoch = function(val){as.POSIXct(val, origin="1970-01-01")}
 
 mentions = data.table(dbGetQuery(conn, "select * from mentions"))
 setnames(mentions, c("source_subr_lwr", "target_subr_lwr"), c("Source", "Target"))
@@ -14,7 +31,7 @@ setkey(mentions, created_utc)
 nodelist = mentions[,union(Source, Target)]
 
 drange = mentions[,list(d_min=min(created_utc), d_max=max(created_utc))]
-drange = lapply(drange, toEpoch)
+drange = lapply(drange, epochToDate)
 drange$d_min = floor_date(drange$d_min, unit="day") + days(1)
 drange$d_max = floor_date(drange$d_max, unit="day")
 
@@ -37,8 +54,11 @@ for(i in 2:length(d_seq)){
 }
 end = Sys.time()
 elaps = end - start
-print(elaps) #
+print(elaps) # 2.8 min to build 43 subgraphs
 
+###################################################
+# Calculate scan statistics for anomaly detection #
+###################################################
 
 start = Sys.time()
 test = scan_stat(graphs, tau=8) # Each graph is 1 week, so this is approx 2 months
@@ -46,34 +66,26 @@ end = Sys.time()
 elaps = end - start
 print(elaps) # 7min
 
+#########################
+# Investigate anomalies #
+#########################
 
 plot(d_seq[-1], test$stat, type='l') # oh baby!
 abline(v=ymd("2015-06-10"), col="red", lty=2) # FPH banned
 abline(v=ymd("2015-07-02"), col='purple', lty=2) # Victoria fired / 2015 Blackout
 #abline(v=ymd("2015-07-10"), col='blue', lty=2)  # Ellen Pao steps down, Huffman appointed
-abline(v=ymd("2015-11-20"), col='blue', lty=2)  # Privacy policy changes announced
+abline(v=ymd("2015-11-20"), col='green', lty=2)  # Privacy policy changes announced
 #abline(v=ymd("2015-08-13"), col='blue', lty=2)  # Reddit bans /r/watchpeopledie in germany
 abline(v=ymd("2015-08-05"), col='blue', lty=2)  # Reddit bans /r/coontown and other racist subs https://www.reddit.com/r/announcements/comments/3fx2au/content_policy_update/ctsqobs ## I'm not convinced this is it.
-
-
-get_peak_details = function(peak_ix){
-  peak_date = d_seq[-1][peak_ix]
-  node_id = test$arg_max_v[peak_ix]
-  node = V(graphs[[peak_ix]])[node_id] # user_history_bot ... wtf? Weirdness.
-  list(date=peak_date, node=node$name)
-}
-
 
 # Identify the most chaotic dates (weeks) and neighborhoods
 details = data.table(t(sapply(which(!is.na(test$stat)), get_peak_details)))
 details$stat = test$stat[!is.na(test$stat)]
 details[order(stat, decreasing=TRUE)]
 
-# pretty sure this returns the size of the giant component
-count_connected_nodes =  function(g){length(component_distribution(g))} 
-n_nodes = sapply(graphs, count_connected_nodes)
-plot(d_seq[-1], n_nodes)
-# It looks like the size of the giant component has actually been *decreasing* over the past year. Very strange.
+#################################
+# Investigate population growth #
+#################################
 
 # Daily count of unique users making subreddit mentioning comments as a proxy
 # for quantifying the size of the active userbase overtime (i.e. is the community shrinking?)
@@ -100,36 +112,105 @@ coef(mod)
 # I should still compare this trend to the global active users to demonstrate that the
 # arrival rate of this particular class of comments is reflective of global user activity.
 
-perc_loss = (active_users$count[-nrow(active_users)] - active_users$count[-1])/active_users$count[-nrow(active_users)]
-plot(perc_loss)
-mod3 = lm(perc_loss~seq_along(perc_loss))
-summary(mod3)
-plot(density(perc_loss[perc_loss>-1 & perc_loss <1])) # noisy fluctuations
-mu = mean(perc_loss[perc_loss>-1 & perc_loss <1])
-# On average, shrinking by about 1.37% per day. 
-y_pred = c(active_users$count[1], active_users$count[-nrow(active_users)]*(1-mu))
-plot(active_users$count, type='l')
-plot(y_pred, col='red')
-xt = seq_along(active_users$count)
-lines(coef(mod)[1]*exp(-(mu)*xt)) # losing a lot fewer than 1.37 perc a day.
 
-mod_exp = lm(log(active_users$count)~active_users$index)
-summary(mod_exp)
-plot(active_users$count, type='l')
-lines(exp(predict(mod_exp)), lty=2, col='red') # much better 
-# reddit is losing about 0.26% of its userbase day-over-day since the beginning of the year.
-# We should try doing a two part model: break it up into chunks for the week before and two
-# weeks after the blackout. 
 pre_blackout  = active_users[date < ymd("2015-06-27")]
 post_blackout = active_users[date > ymd("2015-07-18")]
 
 mod_pre = lm(log(count)~index, pre_blackout)
 mod_post = lm(log(count)~index, post_blackout)
+
+mod_pre_d = lm(log(count)~date, pre_blackout)
+mod_post_D = lm(log(count)~date, post_blackout)
 summary(mod_pre)
 summary(mod_post) # reddit was already shrinking, but accelerated by an order of magnitude after the blackout
 # current shriknage rate = 0.12%
-plot(active_users$count, type='l')
-lines(exp(predict(mod_pre)), lty=2, col='blue')
-lines(post_blackout$index, exp(predict(mod_post)), lty=2, col='red')
+plot(active_users$date, active_users$count,
+     xlab="Date", ylab="#Unique users making 'subreddit mention' comments",
+     main="Effect of the 2015 Blackout on the\nmagnitude of the active reddit user base",
+     type='l'
+     )
+lines(pre_blackout$date, exp(predict(mod_pre_d)), lty=2, col='blue')
+lines(post_blackout$date, exp(predict(mod_post)), lty=2, col='red')
 
 coef(mod_post)/coef(mod_pre) # After the blackout, reddit shrunk by about 2.4% and shrinkage rate increased by 12.5x
+
+#############################################################
+# Investigate community growth (focusing on giant component) #
+#############################################################
+
+# pretty sure this returns the size of the giant component
+count_connected_nodes =  function(g){length(component_distribution(g))} 
+n_nodes = sapply(graphs, count_connected_nodes)
+#plot(d_seq[-1], n_nodes)
+# The giant component is shrinking. 
+# Let's do the same left-right analysis we did for active users
+
+pre_ix  = d_seq < ymd("2015-06-01")
+post_ix = d_seq > ymd("2015-07-04") & d_seq < ymd("2015-12-18")
+n_nodes_pre  = n_nodes[pre_ix]
+n_nodes_post = n_nodes[post_ix]
+
+mod_comp_pre  = lm(n_nodes_pre~which(pre_ix))
+mod_comp_post = lm(n_nodes_post~which(post_ix))
+
+mod_comp_pre_exp  = lm(log(n_nodes_pre)~which(pre_ix))
+mod_comp_post_exp = lm(log(n_nodes_post)~which(post_ix))
+
+plot(d_seq[-1], n_nodes,
+     xlab="Date", ylab="Size of giant component (# nodes)",
+     main="Effect of the 2015 Blackout on the reddit graph"
+     )
+#lines(d_seq[-1][pre_ix], predict(mod_comp_pre), lty=2, col='blue')
+#lines(d_seq[-1][post_ix][-26], predict(mod_comp_post), lty=2, col='red')
+lines(d_seq[-1][pre_ix], exp(predict(mod_comp_pre_exp)), lty=2, col='blue')
+lines(d_seq[-1][post_ix][-26], exp(predict(mod_comp_post_exp)), lty=2, col='red')
+
+# By all accounts, the community is shrinking
+coef(mod_comp_post)/coef(mod_comp_pre) 
+# The community (qua graph) has shrunk about 3% since the blackout
+# It was growing (qua graph) before the black, but it has flipped to shrinking (sign change)
+# and is shrinking over 5x faster than it was previously growing.
+
+# Before the blackout, the main component was growing at a rate of about 19 subreddits per week (+0.09%).
+# Now, we're shrinking at a rate of about 104 subreddits per week (-0.7%).
+
+#####################################################################
+# Compare my user activity rates against the public comment dataset #
+#####################################################################
+
+bq_user_activity = data.table(read.csv('time_series_anomaly_detection/fhoffa-bigquery_2015_active_users_month.csv'))
+bq_user_activity[,month := ymd(paste0(month, "-28"))]
+plot(unique(bq_user_activity), type='l',
+     main="Monthly reddit active users from public comments",
+     xlab="Date", ylab="Unique commenting users"
+     )
+# This plot tells a very different story. Not sure how to reconcile the general, unimpeded growth we're observing
+# here with the reduction in graph-relevant comments
+
+# Th best explanation I can come up with: there general userbase is still growing, but there is a certain class of users,
+# i.e. users who are more likely to make comments containing links to external subreddits, that is participating less
+# since the blackout. The other possible interpretation is that something is discouraging people from making these kinds
+# of comments, but I'm not sure what the mechanism there could be.
+
+mod_bq1 = lm(active_users~month, bq_user_activity[month<ymd("2015-06-29")])
+mod_bq2 = lm(active_users~month, bq_user_activity[month>ymd("2015-06-29")])
+#mod_bq3 = lm(log(active_users)~month, bq_user_activity) # the rate term is so small this is essentially a linear model
+mod_bq3 = lm(active_users~month, bq_user_activity)
+summary(mod_bq3) # .902 adj-r2
+lines(bq_user_activity[month<ymd("2015-06-29"), month], predict(mod_bq1), lty=2, col='blue')
+lines(bq_user_activity[month>ymd("2015-06-29"), month], predict(mod_bq2), lty=2, col='red')
+lines(bq_user_activity$month, predict(mod_bq3), lty=2, col='purple')
+#lines(bq_user_activity$month, exp(predict(mod_bq3)), lty=2, col='purple')
+
+mentions_monthly_activity = mentions[, list(active_users = length(unique(author))), by=list(year, month)]
+mentions_monthly_activity[,date := ymd(paste(year,month, "28", "-"))]
+mentions_monthly_activity[!(month%in%c(2,12,1))][,
+  plot(date, active_users, 
+       type='l',
+       xlab="Date", ylab="Unique users making 'subreddit mention' comments",
+       main="Relative user activity constraining attention to 'subreddit mention' comments"
+       )] 
+# constraining attention to these months looks more like what I was expecting to see, but the fact is: 
+# I really need more complete data here.
+
+# If anything, reddit is growing faster since the blackout.
